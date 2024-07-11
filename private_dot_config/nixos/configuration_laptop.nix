95cace34953b11fe0d67e175f98db67f427967a8
Luca Maff <mafffigo@gmail.com>
2024-07-11 14:34:27 +0200

    Set private key as template

--- private_dot_config/nixos/configuration_laptop.nix
+++ private_dot_config/nixos/configuration_laptop.nix
@@ -115,12 +115,13 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    bitwarden-cli
     bitwarden-desktop
     chezmoi
-    conda
     git
     helix
     jetbrains.pycharm-community
+    lazygit
     logseq
     megasync
     minitube
