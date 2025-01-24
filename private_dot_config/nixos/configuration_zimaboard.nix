d9537a12f564cd7d708877a2440c691e08fa65cf
Luca Maff <mafffigo@gmail.com>
2025-01-24 17:23:30 +0100

    Enable tailscale

--- private_dot_config/nixos/configuration_zimaboard.nix
+++ private_dot_config/nixos/configuration_zimaboard.nix
@@ -79,6 +79,9 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  # Enable tailscale
+  services.tailscale.enable = true;
+
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
